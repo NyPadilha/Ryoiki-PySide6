ba04d650b69f8c11e9c4d52099e930a8f8b84ea0
Nycolas Padilha <763npp@gmail.com>
2024-03-27 13:18:03 -0300

    feat(top_bar): create top bar skeleton

--- gui/windows/main_window/ui_main_window.py
+++ gui/windows/main_window/ui_main_window.py
@@ -38,10 +38,41 @@ class UI_MainWindow(object):
         self.main_layout.setContentsMargins(0, 0, 0, 0)
         self.main_layout.setSpacing(0)
 
-        # title bar
+        # top bar
         self.top_bar = QFrame()
         self.top_bar.setMinimumHeight(30)
         self.top_bar.setMaximumHeight(30)
+
+        self.top_bar_layout = QHBoxLayout(self.top_bar)
+        self.top_bar_layout.setContentsMargins(10, 0, 10, 0)
+        self.top_bar_layout.setSpacing(0)
+
+        self.home_button = QPushButton("Home")
+        self.home_button.setObjectName("home_button")
+        self.home_button.setMaximumWidth(50)
+
+        self.tabs = QFrame()
+        self.tabs.setMinimumHeight(30)
+        self.tabs.setMaximumHeight(30)
+        self.tabs_layout = QHBoxLayout(self.tabs)
+        self.tabs_layout.setContentsMargins(0, 0, 0, 0)
+        self.tabs_layout.setSpacing(0)
+        self.tabs_label = QLabel("Tabs")
+        self.tabs_label.setStyleSheet("color: #FFF")
+        self.tabs_layout.addWidget(self.tabs_label)
+
+        self.plus_button = QPushButton("+")
+        self.plus_button.setObjectName("plus_button")
+        self.plus_button.setMaximumWidth(30)
+
+        self.spacer = QSpacerItem(
+            20, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)  # type: ignore
+
+        # add widgets to top bar layout
+        self.top_bar_layout.addWidget(self.home_button)
+        self.top_bar_layout.addWidget(self.tabs)
+        self.top_bar_layout.addWidget(self.plus_button)
+        self.top_bar_layout.addItem(self.spacer)
 
         # content
         self.content = QFrame()
